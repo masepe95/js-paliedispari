c1e49626f8c936d0e1894e90ec5d14fbc7aadd07
masepe95 <marcosepe10@gmail.com>
2023-06-06 22:58:27 +0200

    palidrom validator added

--- js/script.js
+++ js/script.js
@@ -1,22 +1,30 @@
 console.log('JS OK') 
 
-//** PALIDROMA VALIDATOR **/
+// ! PALIDROMA VALIDATOR /
+// Recupero dal DOM input e button 
+const wordInput = document.getElementById('word');
+const controlButton = document.getElementById('button');
+const resultPlaceholder = document.getElementById('result');
+
+// Creo una funzione che stabilisca se la parola inserita dall'utente è palindroma 
+function isPalindrome(word){
+    for (let i = 0; i < word.length / 2; i++){
+        if (word[i] !== word[word.length - 1 - i]){
+            return 'Non è palindroma';
+        }
+    }
+
+    return 'Si, è palindroma';
+    
+}
+
+// Creo un event listener che raccoglie il value dell'input dove l'utente inserisce la parola 
+controlButton.addEventListener('click', function(){
+    const word = wordInput.value;
+    const result = isPalindrome(word);
+    console.log(result)
+    resultPlaceholder.innerHTML = result
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//** EVEN OR ODD **/
+// !  EVEN OR ODD /
